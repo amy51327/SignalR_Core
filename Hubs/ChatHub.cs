e82e65327a952430ba66d4359edfdef756b9c39a
Amie_Chen <Amie_Chen@compal.com>
2021-05-28 22:32:22 +0800

    add signalr sample with js

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR_Core.Hubs
+{    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessaasdsge", user, message);
+        }
+
+    }
+}
